b543c9296a264c04b67e0d8183ca5f2ab13e1346
Emelia Mavis <emelia.mavis@duke.edu>
2021-11-09 14:57:47 -0500

    Added code for merging shipment data and created fully merged data set (Death_Population_Shipment

--- 10_code/merge_population_death_shipments.py
+++ 10_code/merge_population_death_shipments.py
@@ -9,14 +9,13 @@ shipments = pd.read_csv(
 )
 
 # Rename shipments columns to match population_death
-shipments.rename(
+shipments = shipments.rename(
     {"BUYER_STATE": "State", "BUYER_COUNTY": "County", "YEAR": "Year"},
     axis=1,
-    inplace=True,
-)
+).copy()
 
 # Drop unnecessary columns
-shipments.drop(labels=["Unnamed: 0", "Unnamed: 0.1"], axis=1, inplace=True)
+shipments = shipments.drop(labels=["Unnamed: 0", "Unnamed: 0.1"], axis=1).copy()
 
 # Change County entries to sentence-case
 counties = list(shipments.County)
@@ -26,6 +25,12 @@ shipments.County = new_counties
 # Change types to match
 shipments.Year = shipments.Year.astype("float64")
 
+# Modify county observations to include "County"
+shipments["temp_county"] = ""
+shipments.temp_county = [shipments.County[i] + " County" for i in range(len(shipments))]
+shipments = shipments.drop(labels="County", axis=1).copy()
+shipments = shipments.rename({"temp_county": "County"}, axis=1).copy()
+
 # Great, now can merge on state-county-year
 merged = pd.merge(
     population_death,
@@ -33,4 +38,16 @@ merged = pd.merge(
     on=["Year", "State", "County"],
     validate="1:1",
     indicator=True,
+)
+
+# last validity checks
+assert ~merged.isna().any().sum()
+assert ~merged.duplicated(["Year", "County", "State"]).any().sum()
+
+# write output file
+merged.to_csv(
+    "~/720/pds2021-opioids-team-8-1/20_intermediate_files/Death_Population_Shipments.csv"
+)
+merged.to_parquet(
+    "~/720/pds2021-opioids-team-8-1/20_intermediate_files/Death_Population_Shipments.gzip"
 )
